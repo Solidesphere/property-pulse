5547b277d87db0547c9166ec1ed2ea9170e20d78
Kahil Mohamed Amine <kahil.mohamed.amine@etu.univ-blida.dz>
2024-09-28 18:43:49 +0100

    Models Property & User

---
+++ config/database.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+let connected = false;
+
+const connectDB = async () => {
+  mongoose.set("strictQuery", true);
+  // if the connection is already established dont connect again
+  if (connected) {
+    console.log("mongoose is connected");
+    return;
+  }
+  // connect to mongodb
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    connected = true;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectDB;
---
+++ models/Property.js
@@ -0,0 +1,78 @@
+import { Schema, model, models } from "mongoose";
+
+const propertySchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    type: {
+      type: String,
+      required: [true, "type is required"],
+    },
+    description: {
+      type: String,
+    },
+    location: {
+      street: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      zipcode: {
+        type: String,
+        required: true,
+      },
+    },
+    beds: {
+      type: Number,
+      required: true,
+    },
+    baths: {
+      type: Number,
+      required: true,
+    },
+    square_feet: {
+      type: Number,
+      required: true,
+    },
+    amenities: [{ type: String }],
+    rates: {
+      nightly: Number,
+      weekly: Number,
+      monthly: Number,
+    },
+    seller_info: {
+      name: String,
+      email: String,
+      phone: String,
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    is_featured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Property = models.Property || model("Property", propertySchema);
+export default Property;
