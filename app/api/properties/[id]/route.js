74afe94e1e318718c996c0ff89392eb4d3d9a21e
solide <solidesphere@gmail.com>
2025-02-11 15:28:25 +0100

    adding api route

---
+++ app/api/properties/[id]/route.js
@@ -0,0 +1,22 @@
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+
+export const GET = async (request, { params }) => {
+  try {
+    await connectDB();
+    const property = await Property.findById(params.id);
+
+    if (!property) {
+      return new Response(JSON.stringify({ error: "property not found" }), {
+        status: 404,
+      });
+    }
+    return new Response(JSON.stringify(property), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "somthing went wrong " }), {
+      status: 500,
+    });
+  }
+};
